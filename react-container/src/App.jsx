cd681505302d8c2b642dff0860da4e2462b8b040
priyanka56571 <priyankathulkar123@gmail.com>
2025-01-11 18:21:07 +0530

    first commit

---
+++ react-container/src/App.jsx
@@ -0,0 +1,69 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import Carousel from 'react-bootstrap/Carousel';
+import Layout from './Layout';
+import Cards from './Cards';
+
+
+import im1 from "./images/img1.jpeg";
+import im2 from "./images/img2.jpeg";
+import im3 from "./images/img3.jpeg";
+
+
+
+const App = () => {
+  return (
+    <>
+      <h1>hello priyanka </h1>
+
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+
+      <Carousel>
+      <Carousel.Item>
+        {/* <ExampleCarouselImage text="First slide" /> */}
+        <img src={im1} alt="" style={{height:"300px",width:"100%"}} />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        {/* <ExampleCarouselImage text="Second slide" /> */}
+        <img src={im2} alt="" style={{height:"300px",width:"100%"}} />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        {/* <ExampleCarouselImage text="Third slide" /> */}
+        <img src={im3} alt="" style={{height:"300px",width:"100%"}} />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    <Layout/>
+    <Cards/>
+
+
+    </>
+  )
+}
+
+export default App
